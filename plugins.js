c0daa6b811021cc5113a9c2e12100e420d0f02e6
ahmedVadim <ahmedivana179@gmail.com>
2016-09-24 15:36:12 +0300

    Loading Screen...

--- plugins.js
+++ plugins.js
@@ -16,4 +16,24 @@ colorLi.click(function()
 {
   $("link[href*='theme']").attr("href", $(this).attr("data-value"));
 });
+
+// Loading Screen
+
+$(window).load(function () {
+
+    "use strict";
+
+    // Loading Elements
+
+    $(".loading-overlay .spinner").fadeOut(2000, function () {
+
+        // Show The Scroll
+
+        $("body").css("overflow", "auto");
+
+        $(this).parent().fadeOut(2000, function () {
+
+            $(this).remove();
+        });
+    });
 });
